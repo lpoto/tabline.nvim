4a0d05d7e8d1ae64f125070062926032660f4d74
lpoto <luka.potocnik@icloud.com>
2023-11-25 15:07:29 +0100

    fix: builtin.diagnostic_error showing warnings

--- lua/tabline/builtin.lua
+++ lua/tabline/builtin.lua
@@ -38,7 +38,7 @@ end
 function builtin.diagnostic_error(opts)
   if type(opts) ~= 'table' then opts = {} end
   local n = #vim.diagnostic.get(opts.buf, {
-    severity = vim.diagnostic.severity.WARN,
+    severity = vim.diagnostic.severity.ERROR,
   })
   return n > 0 and ('E: %d'):format(n) or ''
 end
